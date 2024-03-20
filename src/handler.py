0d0fdfed353ded96b22e9eb9dda856a9965ece65
Victor Aguilar C. <vicobits@gmail.com>
2024-03-20 15:06:58 -0600

    fix: add warmup

--- src/handler.py
+++ src/handler.py
@@ -1,4 +1,10 @@
+import lambdawarmer
 from mangum import Mangum
 from src.fastapi_app import app
 
 handler = Mangum(app=app)
+
+
+@lambdawarmer.warmer
+def wrapper(event, context):
+    return handler(event, context)
