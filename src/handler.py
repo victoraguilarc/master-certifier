2a7e15a7294bb74e13e0114099c34618b6301c7d
Victor Aguilar C. <vicobits@gmail.com>
2024-03-21 11:54:25 -0600

    fix: warmup

--- src/handler.py
+++ src/handler.py
@@ -1,10 +1,12 @@
-import lambdawarmer
 from mangum import Mangum
+from mangum.types import LambdaContext
+
 from src.fastapi_app import app
 
 handler = Mangum(app=app)
 
 
-@lambdawarmer.warmer
-def wrapper(event, context):
+def wrapped_handler(event: dict, context: LambdaContext):
+    if event.get('source') == 'serverless-plugin-warmup':
+        return
     return handler(event, context)
